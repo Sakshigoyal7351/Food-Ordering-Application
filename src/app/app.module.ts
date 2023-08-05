25f44da9b183ee115d7626ab32fcacbf5460c066
sakshi <sakshigoyal7351@gmail.com>
2023-08-05 19:16:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/partials/header/header.component';
+import { HomeComponent } from './component/pages/home/home.component';
+import { SearchComponent } from './component/partials/search/search.component';
+import { FoodPageComponent } from './component/pages/food-page/food-page.component';
+import { TagsComponent } from './component/partials/tags/tags.component';
+import { CartPageComponent } from './component/pages/cart-page/cart-page.component';
+import { TitleComponent } from './component/partials/title/title.component';
+
+// import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    FoodPageComponent,
+    TagsComponent,
+    CartPageComponent,
+    TitleComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
